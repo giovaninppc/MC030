737b08cabcdfa175113e8069fc2084355ca49686
Jun <ra177677@students.ic.unicamp.br>
2019-10-14 20:51:23 -0300

    fixing index.js

--- server/index.js
+++ server/index.js
@@ -2,74 +2,35 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-
-
-
-
-
-
-
-
   res.send('Hello World!');
 });
 
-// Function callName() is executed whenever  
-// url is of the form localhost:3000/name 
-app.get('/name', callName); 
-  
-function callName(req, res) { 
-      
-    // Use child_process.spawn method from  
-    // child_process module and assign it 
-    // to variable spawn 
-    var spawn = require("child_process").spawn; 
-      
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
-      
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-    // so, first name = Mike and last name = Will 
-    var process = spawn('python',["./healthcheck.py", 
-                            req.query.firstname, 
-                            req.query.lastname] ); 
-  
-    // Takes stdout data from script which executed 
-    // with arguments and send this data to res object 
-    process.stdout.on('data', function(data) { 
-        res.send(data.toString()); 
-    } ) 
-} 
+app.get('/name', callName);
 
-// Function callName() is executed whenever  
-// url is of the form localhost:3000/name 
-app.get('/main', callName); 
-  
-function callName(req, res) { 
-      
-    // Use child_process.spawn method from  
-    // child_process module and assign it 
-    // to variable spawn 
-    var spawn = require("child_process").spawn; 
-      
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
-      
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-    // so, first name = Mike and last name = Will 
-    var process = spawn('python',["main.py", 
-                            req.query.firstname, 
-                            req.query.lastname] ); 
-  
-    // Takes stdout data from script which executed 
-    // with arguments and send this data to res object 
-    process.stdout.on('data', function(data) { 
-        res.send(data.toString()); 
-    } ) 
-} 
+function callName(req, res) {
+
+  // Use child_process.spawn method from  
+  // child_process module and assign it 
+  // to variable spawn 
+  var spawn = require("child_process").spawn;
+
+  // Parameters passed in spawn - 
+  // 1. type_of_script 
+  // 2. list containing Path of the script 
+  //    and arguments for the script  
+
+  // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+  // so, first name = Mike and last name = Will 
+  var process = spawn('python', ["./healthcheck.py",
+    req.query.firstname,
+    req.query.lastname]);
+
+  // Takes stdout data from script which executed 
+  // with arguments and send this data to res object 
+  process.stdout.on('data', function (data) {
+    res.send(data.toString());
+  })
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
