7c6fecfc7ed2a683bed2bec2e2949f13d0300059
MrLeonardDude <leopagnez@gmail.com>
2019-10-20 23:08:16 -0300

    Inital Commit Flask Implementation
    Hello World Flask-UWSGI-Nginx with bult in image.

--- server_python/app/main.py
+++ server_python/app/main.py
@@ -1,11 +1,14 @@
+from flask import Flask
 from video import *
 from image import *
 from downloader import *
 from arguments import *
 
-def processImage(path):
-    img_processor = ImageProcessor()
-    img_processor.detectObjectsFromImage(path)
+
+def downloadVideo(path):
+    pytube = PytubeDownloader()
+    pytube.downloadVideo(path)
+
 
 def processVideo(path, args):
     if args.frameByFrame:
@@ -13,45 +16,39 @@ def processVideo(path, args):
     else:
         processEntireVideo(path)
 
+
 def processEntireVideo(path):
     processor = VideoProcessor()
     processor.detectObjectsFromVideo(path)
 
+
 def processVideoFrameByFrame(path):
     detectedObjects = []
-
     img_processor = ImageProcessor()
     frame_extractor = FrameExtractor(path)
-
     while frame_extractor.getNextFrame():
         objects = img_processor.detectObjectsFromImage('frame.png')
         detectedObjects += objects
-
     dict = {}
     for object in detectedObjects:
         previousValue = dict.get(object) or 0
         dict[object] = previousValue + 1
-
     print(dict)
+
 
 def downloadVideo(path):
     pytube = PytubeDownloader()
     pytube.downloadVideo(path)
 
+app = Flask(__name__)
+
+
+@app.route("/<url>")
+def hello():
+    return "Hello World from Flask in a uWSGI Nginx Docker container with \
+     Python 3.7 (from the example template)"
+
 
 if __name__ == "__main__":
-    # Run arguments
-    configuration = setupArguments()
-    path = configuration.path
-    args = configuration.args
-
-    if args.image:
-        processImage(path)
-
-    if args.video:
-        processVideo(path, args)
-
-    elif args.youtube:
-        downloadVideo(path)
-        processVideo('temp.mp4')
-        os.remove('temp.mp4')
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
