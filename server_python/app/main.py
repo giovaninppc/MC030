bb0b5aa67670f48f3f07c1309912b4bdd32810fb
Jun <ra177677@students.ic.unicamp.br>
2019-10-25 21:48:07 -0300

    writing on output file with time stamps

--- server_python/app/main.py
+++ server_python/app/main.py
@@ -1,16 +1,21 @@
 from flask import Flask
 import utils
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route("/<url>/<frame_by_frame_flag>")
 def hello(url, frame_by_frame_flag):
+    gotAt = datetime.now().strftime('%M:%S.%f')
+    print(f"{gotAt},", file=open(name, "a"), end="")
     utils.downloadVideo(f'https://www.youtube.com/watch?v={url}')
     utils.processVideo('temp.mp4', frame_by_frame_flag)
-    return f"DOING YOUR SHIT at https://www.youtube.com/watch?v={url}"
-
+    processedAt = datetime.now().strftime('%M:%S.%f')
+    print(f"\nfinished at: {processedAt}\n")
+    name = f"out.txt"
+    print(f"{processedAt}", file=open(name, "a"))
+    return f"Got requisition at: {gotAt}"
 
 if __name__ == "__main__":
     # Only for debugging while developing
