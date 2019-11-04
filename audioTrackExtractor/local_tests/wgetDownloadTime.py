3ba2c46a33299b660c531752f6ac2cbb54e58bf2
Bianca Itiroko <bianca.itiroko@ifood.com.br>
2019-11-04 20:42:46 -0300

    Fixing time logging on csv

--- audioTrackExtractor/local_tests/wgetDownloadTime.py
+++ audioTrackExtractor/local_tests/wgetDownloadTime.py
@@ -1,11 +1,18 @@
 import sys
 import os
-from datetime import datetime
+import datetime
 
 def appendOnTextFile(path, text):
     file = open(path, 'a+')
     file.write(text)
     file.close()
+
+def betweenTwoDates(a, b):
+	# Create datetime objects for each time (a and b)
+	dateTimeA = datetime.combine(datetime.now().date(), a)
+	dateTimeB = datetime.combine(datetime.now().date(), b)
+	# Get the difference between datetimes (as timedelta)
+	dateTimeDifference = dateTimeA - dateTimeB
 
 print("Dowload time measure")
 print('Run ' + sys.argv[1] + ' times')
@@ -14,22 +21,27 @@ file = sys.argv[2]
 
 downloadStart = 0.0
 downloadStop = 0.0
+
 logPath = "wget_download_time.csv"
 
-appendOnTextFile(logPath, "download start, download stop")
+cmd = 'rm ' + logPath
+os.system(cmd)	
+
+appendOnTextFile(logPath, "download start, download stop\n")
 
 for i in range(0, int(sys.argv[1])):
     print('Execution nº: ' + str(i))
-    downloadStart = datetime.now().time()
+    downloadStart = datetime.datetime.now()
     cmd = 'wget http://www.students.ic.unicamp.br/\~ra177677/' + file
-    
     os.system(cmd)
-    downloadStop = datetime.now().time()
+    downloadStop = datetime.datetime.now()
+    duration = downloadStop - downloadStart
+    duration = duration.microseconds
 
     cmd = 'rm ' + file
     os.system(cmd)
 
-    log = '{}, {}\n'.format(str(downloadStart), str(downloadStop))
+    log = '{}, {}, {}\n'.format(str(downloadStart), str(downloadStop), str(duration))
     appendOnTextFile(logPath, log)
 
     print('{}\t{}\t'.format(str(downloadStart), str(downloadStop)))
