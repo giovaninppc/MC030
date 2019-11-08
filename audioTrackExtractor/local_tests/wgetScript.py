df342e7d494820a4c819a8c9562b26e1f2e9b6d3
Bianca Itiroko <bianca.itiroko@ifood.com.br>
2019-11-08 20:30:09 -0300

    adding script - for real

---
+++ audioTrackExtractor/local_tests/wgetScript.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+print('Run ' + sys.argv[1] + ' times')
+
+
+files = ['10s.mp4', '20s.mp4', '45s.mp4', '60s.mp4', '90s.mp4', '120s.mp4', '150s.mp4', '180s.mp4', '300s.mp4', '420s.mp4', '600s.mp4']
+
+for file in files:
+	outputFileName = "wgetCSV_"+file
+	for i in range(0, int(sys.argv[1])):
+		print('Execution nº: ' + str(i))
+		cmd = 'python3 wgetDownloadTime.py '+sys.argv[1]+" "+ file + " " + outputFileName+".csv"
+		os.system(cmd)
