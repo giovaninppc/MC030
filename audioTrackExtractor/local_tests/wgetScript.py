da8006172dfd03bae0c46f40631560c151c52d5d
Jun <ra177677@students.ic.unicamp.br>
2019-11-15 11:04:45 -0300

    updating wgetScript.py

--- audioTrackExtractor/local_tests/wgetScript.py
+++ audioTrackExtractor/local_tests/wgetScript.py
@@ -3,6 +3,6 @@ import os
 print('Run ' + sys.argv[1] + ' times')
 files = ['10s.mp4', '20s.mp4', '45s.mp4', '60s.mp4', '90s.mp4', '120s.mp4', '150s.mp4', '180s.mp4', '300s.mp4', '420s.mp4', '600s.mp4']
 for file in files:
-        outputFileName = "wgetCSV_"+file
-		cmd = 'python3 wgetDownloadTime.py '+sys.argv[1]+" "+ file + " " + outputFileName+".csv"
-		os.system(cmd)
+	outputFileName = "wgetCSV_"+file
+	cmd = 'python3 wgetDownloadTime.py '+sys.argv[1]+" "+ file + " " + outputFileName+".csv"
+	os.system(cmd)
