b6e6c55c0ef82b42d49c9577a63cd6d0cc97b9d4
Jun <ra177677@students.ic.unicamp.br>
2019-11-15 10:31:30 -0300

    adding youtube urls

--- audioTrackExtractor/local_tests/youtubeDownloadScript.py
+++ audioTrackExtractor/local_tests/youtubeDownloadScript.py
@@ -3,7 +3,17 @@ import os
 
 print('Run ' + sys.argv[1] + ' times')
 
-urls = ["https://www.youtube.com/watch?v=EK44YUWxo0I"]
+urls = ["https://www.youtube.com/watch?v=q-xSQ7MU-K8",
+"https://www.youtube.com/watch?v=FNaM4qIKXc8",
+"https://www.youtube.com/watch?v=fvE38raWAMg",
+"https://www.youtube.com/watch?v=Nn9KqKMFhow",
+"https://www.youtube.com/watch?v=XagMIeb3PyY",
+"https://www.youtube.com/watch?v=m0GufRJYxfQ",
+"https://www.youtube.com/watch?v=RLekLicU3RA",
+"https://www.youtube.com/watch?v=cHeGWCKRnl8",
+"https://www.youtube.com/watch?v=IzRwagNhUKo",
+"https://www.youtube.com/watch?v=-hH-WtPlUUY",]
+
 k = 0
 for url in urls:
 	k = k + 1
